8425f281602756a170bf6bdf761db0da58581c50
Nick <nickq4504@gmail.com>
2023-08-28 16:06:07 -0230

    -More pathes added -random encounter functionality created for when the player decides to investigate the plaza in the settlement.

--- pathes.py
+++ pathes.py
@@ -1,3 +1,5 @@
+import random
+
 class TreeNode:
   def __init__(self, text, choices=None):
     self.text = text
@@ -6,10 +8,18 @@ class TreeNode:
 
 def story_pathes():
   #Create the story nodes and choices
-  choice2a = TreeNode("You explore the nearby settlement")
-  choice2b = TreeNode("You explore the wildrness")
-  choice2c = TreeNode("You choose to go deeper into the space station")
-  choice2d = TreeNode("You decide to explore the hangar area")
+  choice3a = TreeNode("You investigate the plaza") #Random encounter here
+  choice3b = TreeNode("You investigate a building")
+  choice3c = TreeNode("You take the left path")
+  choice3d = TreeNode("You take the right path")
+  choice3e = TreeNode("You go to the upper floor")
+  choice3f = TreeNode("You go to the lower floor")
+  choice3g = TreeNode("You investigate one of the old ships")
+  choice3h = TreeNode("You investigate the storage area in the hangar")
+  choice2a = TreeNode("You explore the nearby settlement", [choice3a, choice3b])
+  choice2b = TreeNode("You explore the wildrness", [choice3c, choice3d])
+  choice2c = TreeNode("You choose to go deeper into the space station", [choice3e, choice3f])
+  choice2d = TreeNode("You decide to explore the hangar area", [choice3g, choice3h])
   choice1a = TreeNode("Explore the planet", [choice2a, choice2b])
   choice1b = TreeNode("Explore the spacestation", [choice2c, choice2d])
   root = TreeNode("You enter the Theta system. ahead of you is a planet and a spacestation", [choice1a, choice1b])
@@ -21,16 +31,33 @@ def story_pathes():
   choice2b.parent = choice1a
   choice2c.parent = choice1b
   choice2d.parent = choice1b
+  choice3a.parent = choice2a
+  choice3b.parent = choice2a
+  choice3c.parent = choice2b
+  choice3d.parent = choice2b
+  choice3e.parent = choice2c
+  choice3f.parent = choice2c
+  choice3g.parent = choice2d
+  choice3h.parent = choice2d
+
 
 
   #Add random encounters
+  choice3a.encounter_rate = 3
 
   #Add chance of finding random resource
 
+  #Add
+
   return root
 
-def get_input():
-  pass
+def encounter_plaza(choice):
+  chance = random.randint(1,10)
+  if choice.text == "You investigate the plaza" and chance == 8:
+    print("You encounter a robot")
+  else:
+    print("The coast is clear")
+
 
 
 def show_choices(choices):
@@ -43,7 +70,7 @@ def main():
   story_root = story_pathes()
   current_node = story_root
 
-  print("---Welcome to the choose your own adventure game!---")
+  print("---Welcome to the Space Exploration game!---")
   while current_node.choices:
     print(current_node.text)
     show_choices(current_node.choices)
@@ -52,6 +79,8 @@ def main():
       choice = int(input("Enter the number of your choice:  "))
       if 1 <= choice <= len(current_node.choices):
         current_node = current_node.choices[choice - 1]
+        if current_node.text == "You investigate the plaza":
+          encounter_plaza(current_node)
       else:
         print("Attention! Invalid input. Please try again.")
     except ValueError:
