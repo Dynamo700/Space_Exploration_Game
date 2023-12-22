edff78fdefea3f3d0392f83c151f904c837c61a0
Nick <nickq4504@gmail.com>
2023-12-21 23:48:48 -0330

    -Chances of encounters changed.

--- encounters.py
+++ encounters.py
@@ -1,7 +1,7 @@
 import random
 def encounter(choice):
-  chance = random.randint(1,10)
-  if choice.text == "You investigate the plaza" and chance == 8:
+  chance = random.randint(1,8)
+  if choice.text == "You investigate the plaza" and chance == 5:
     print("You encounter a robot")
   elif choice.text == "You take the left path" and chance == 3:
     print("You encounter a beast")
