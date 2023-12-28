b97dee97f4e66445629bfd184fbd224caf769473
Nick <nickq4504@gmail.com>
2023-12-27 20:57:08 -0330

    -Event text changed

--- events.py
+++ events.py
@@ -6,5 +6,7 @@ def game_events(choice):
     print("Suddenley the writing begins to glow blue, and energy forms around you, as you are teleported away from the planet...")
   elif choice.text == "You approach the statue":
     print("The statue was nearly 10 feet tall and seems to be humanoid shaped but has a wolf's head, hands and feet")
-    print("You hear faint...Whispering as you approach.")
-    print("Suddenley the statue moves toward you, it's eyes glowing blue, it presentes you with a mysterious sword.")
+    print("You hear faint whispering as you approach...")
+    print("Suddenley the statue moves toward you, it's eyes glowing blue, it presents you with a mysterious sword!")
+    print("The sword has a blade that glows teal. The hilt is made from a lustrious metal. It is possible it is not of this galaxy")
+    print("It seems you may have been chosen for a higher purpose.")
